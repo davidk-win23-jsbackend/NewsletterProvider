8240bd47198eccc61e16954c86db146f72a9508a
DaveElop <dkomnenovic@live.com>
2024-05-30 02:36:13 +0200

    Add project files.

---
+++ Data/Entities/SubscribeEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities;
+
+public class SubscribeEntity
+{
+    [Key]
+    public string Email { get; set; } = null!;
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekinReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+}
---
+++ Data/Factories/DataContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Contexts;
+
+namespace Data.Factories;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Server=tcp:siliconsql.database.windows.net,1433;Initial Catalog=silicon-database;Persist Security Info=False;User ID=SqlAdmin;Password=Sifra123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
